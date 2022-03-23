3f5648c807e39699e96b5c771b82b87b4431c646
kutakieu <kutakieu22@gmail.com>
2022-03-23 10:38:48 +0900

    update dataset, dataloader

--- dataloader.py
+++ dataloader.py
@@ -1,16 +1,31 @@
+from pathlib import Path
+
 from pytorch_lightning import LightningDataModule
-from torchvision import transforms
+from torch.utils.data import DataLoader, random_split
+
+from dataset import DepthDataset
 
 
 class DepthDataModule(LightningDataModule):
+    def __init__(self, rgb_dir: str, depth_dir: str, batch_size: int = 8, train_val_test_ratio = [0.7, 0.1, 0.2]):
+        super().__init__()
+        self.rgb_dir = Path(rgb_dir)
+        self.depth_dir = Path(depth_dir)
+        self.batch_size = batch_size
+        self.train_val_test_ratio = train_val_test_ratio
+
     def prepare_data(self):
-        pass
+        depth_dataset = DepthDataset(self.rgb_dir, self.depth_dir)
+        n_train_samples = len(depth_dataset) * (self.train_val_test_ratio[0] / sum(self.train_val_test_ratio))
+        n_val_samples = len(depth_dataset) * (self.train_val_test_ratio[1] / sum(self.train_val_test_ratio))
+        n_test_samples = len(depth_dataset) - (n_train_samples + n_val_samples)
+        self.train_dataset, self.val_dataset, self.test_dataset =  random_split(depth_dataset, [n_train_samples, n_val_samples, n_test_samples])
 
     def train_dataloader(self):
-        pass
+        DataLoader(self.train_dataset, batch_size=self.batch_size)
 
     def val_dataloader(self):
-        pass
+        DataLoader(self.val_dataset, batch_size=self.batch_size)
     
     def test_dataloader(self):
-        pass
+        DataLoader(self.test_dataset, batch_size=self.batch_size)
--- dataset.py
+++ dataset.py
@@ -1,24 +1,25 @@
-import os
+from pathlib import PosixPath
 
 from torch.utils.data import Dataset
 from torchvision.io import read_image
 
 
 class DepthDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
-        self.img_labels = pd.read_csv(annotations_file)
-        self.img_dir = img_dir
+    def __init__(self, rgb_dir: PosixPath, depth_dir: PosixPath, transform=None, target_transform=None):
+        self.rgb_dir = rgb_dir
+        self.depth_dir = depth_dir
         self.transform = transform
         self.target_transform = target_transform
+        self.rgb_paths = [file for file in self.rgb_dir.glob("*") if not str(file.name).startswith(".")]
 
     def __len__(self):
-        return len(self.img_labels)
+        return len(self.rgb_paths)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = read_image(img_path)
-        label = self.img_labels.iloc[idx, 1]
-        if self.transform:
-            for transform in self.transforms:
-            image = self.transform(image)
-        return image, label
+        rgb_path = self.rgb_paths[idx]
+        depth_path = self.depth_dir / f"{self.rgb_paths[idx].stem}.png"
+        rgb_img = read_image(rgb_path)
+        depth_img = read_image(depth_path)
+        rgb_img = self.transform(rgb_img)
+        depth_img = self.target_transform(depth_img)
+        return rgb_img, depth_img
